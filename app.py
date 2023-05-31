43606ac0a7d73c87d49e714375e412730dd22f27
Chandan Chandel <76410929+cchandel-dev@users.noreply.github.com>
2023-05-30 23:43:28 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+
+from flask import Flask
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+app = Flask(__name__)
+#MongoDB user permissions chaged to modify all databases
+uri = "mongodb+srv://Brain3DVizMember:9GyKqp4b9blclzqJ@tinyurl-experimental.cuym0r0.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+db = client['tinyURL-experimental'] #Select the database
+cl = db['test'] #Select the collection name
+
+@app.route('/')
+def index():
+    # Store data in MongoDB
+    cl.insert_one({'name': 'John', 'age': 25})
+    return 'Data stored in MongoDB!'
+
+if __name__ == '__main__':
+    app.run()
