abd5871e0cfeecee702e65e85cc577c295f0ed59
Chandan Chandel <76410929+cchandel-dev@users.noreply.github.com>
2023-05-31 10:53:46 -0400

    Page Rendering enabled

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-app = Flask(__name__)
+
 #MongoDB user permissions chaged to modify all databases
 uri = "mongodb+srv://Brain3DVizMember:9GyKqp4b9blclzqJ@tinyurl-experimental.cuym0r0.mongodb.net/?retryWrites=true&w=majority"
 
@@ -18,11 +18,13 @@ except Exception as e:
 db = client['tinyURL-experimental'] #Select the database
 cl = db['test'] #Select the collection name
 
+app = Flask(__name__)
+
 @app.route('/')
 def index():
     # Store data in MongoDB
     cl.insert_one({'name': 'John', 'age': 25})
-    return 'Data stored in MongoDB!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
