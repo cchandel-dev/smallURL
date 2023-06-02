c1a355b95b027e0a30795af93f5072a11e024aef
Chandan Chandel <76410929+cchandel-dev@users.noreply.github.com>
2023-06-01 23:07:50 -0400

    encoding works
    stores results in db and gives them back without overwriting

--- app.py
+++ app.py
@@ -2,7 +2,9 @@
 from flask import Flask, render_template, request, jsonify, session
 import requests
 from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from pymongo.server_api import ServerApi        
+import hashlib
+import base64
 
 
 #MongoDB user permissions chaged to modify all databases
@@ -24,7 +26,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Store data in MongoDB
-    cl.insert_one({'name': 'John', 'age': 25})
+    cl.insert_one({'long_link': 'John', 'short_link': ''})
     return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
@@ -35,7 +37,7 @@ def process():
         try:
             response = requests.head(link)
             if response.status_code == requests.codes.ok:
-                response = 'Link is valid' #encode called from here
+                response = encode(link) #encode called from here
             else:
                 response = 'Link is not valid'
         except requests.exceptions.RequestException:
@@ -50,6 +52,36 @@ def process():
     response = {'output': response}
 
     return jsonify(response)
+
+
+def encode(link):
+    # String to search for
+
+    # Query the collection to check if the string exists
+    result = cl.find_one({"long_link": link})
+
+    if result is not None:
+        short_link = 'from db: ' + result["short_link"]
+        return short_link
+    else:
+        short_link = generate_unique_string(link)
+        cl.insert_one({'long_link': link, 'short_link': short_link})
+        return 'from encoder: ' + short_link
+
+
+
+def generate_unique_string(input_string):
+    # Hash the input string using SHA-256
+    hashed = hashlib.sha256(input_string.encode()).digest()
+
+    # Take the first 6 bytes of the hash and encode it in base64
+    encoded = base64.b64encode(hashed[:6]).decode()
+
+    # Take the first 7 characters of the encoded string
+    unique_string = encoded[:7]
+
+    return unique_string
+
 
 if __name__ == '__main__':
     app.run(debug = True)
